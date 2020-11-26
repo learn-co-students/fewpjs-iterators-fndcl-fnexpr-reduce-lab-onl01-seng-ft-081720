faf094068adcdb5e5537ae50ed1c3a9312b9e670
Eli Cusic <tntitans31@gmail.com>
2020-11-26 10:56:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// function batteries(arr){ 
+//     const totalBatteries = arr.reduce(function(x, total){
+//         return x += y
+//     })
+//     return totalBatteries
+// }
+
+const totalBatteries = batteryBatches.reduce(function(x, y){
+    return x += y
+})
+console.log(totalBatteries)
